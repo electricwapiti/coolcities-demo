bbd2621ac0d43df4e03aed124c04bd90ab6c783a
Jack Conneely <jack@conneely.org>
2026-08-19 07:38:46 -0500

    select homepage

--- src/navigation.ts
+++ src/navigation.ts
@@ -6,20 +6,8 @@ export const headerData = {
       text: 'Homes',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
           text: 'Startup',
           href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
         },
       ],
     },
